5000545a594ca4f5724c3aef502a1c21ad30f4bc
Gede Wiguna <wiguna.gd@gmail.com>
2026-02-04 20:02:51 +0800

    2026-02-04

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -134,7 +134,14 @@ const Detail = () => {
                   {likeCount ?? dataPostDetail?.likes}
                 </Link>
 
-                <Link href="#" onClick={() => setIsDialogCommentOpen(!isDialogCommentOpen)} className="flex items-center gap-2 text-sm">
+                <Link
+                  href="#commentSection"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    const element = document.getElementById('commentSection');
+                    element?.scrollIntoView({ behavior: 'smooth' });
+                  }}
+                  className="flex items-center gap-2 text-sm">
                   <Image
                     src={iconComment}
                     width={20}
@@ -168,7 +175,7 @@ const Detail = () => {
 
 
               <div className="flex flex-col gap-5 border-t border-b py-5">
-                <h3 className="text-xl font-bold">Comment ({dataComments?.length})</h3>
+                <h3 id="commentSection" className="text-xl font-bold">Comment ({dataComments?.length})</h3>
 
                 {!isuser && (
                   <LoginBeforeComment pathname={pathname} />
@@ -205,7 +212,7 @@ const Detail = () => {
                   </div>
                 )}
 
-                <div className="grid w-full justify-start">
+                <div className="flex flex-col w-full justify-start">
 
                   {dataComments && dataComments.length > 0 && (
                     dataComments?.slice(0, 3).map((comment) => (
@@ -232,84 +239,82 @@ const Detail = () => {
                     ))
                   )}
 
-                  {(dataComments?.length ?? 0) > 0 && (
-                    <div className="flex gap-1 items-center">
-                      <Dialog open={isDialogCommentOpen} onOpenChange={() => setIsDialogCommentOpen(!isDialogCommentOpen)}>
-                        <DialogTrigger asChild>
-                          <Button variant="link" className="font-bold underline p-0">
-                            See All Comments
-                          </Button>
-                        </DialogTrigger>
-                        <DialogContent className="md:max-w-[613px] md:max-h-[902px] h-[90vh] flex flex-col p-0 gap-0 overflow-hidden">
-                          <DialogHeader className="p-6 pb-2 flex-none">
-                            <DialogTitle className="text-xl font-bold">
-                              Comment({dataComments?.length || 0})
-                            </DialogTitle>
-                          </DialogHeader>
+                  <div className="flex gap-1 items-center">
+                    <Dialog open={isDialogCommentOpen} onOpenChange={() => setIsDialogCommentOpen(!isDialogCommentOpen)}>
+                      <DialogTrigger asChild>
+                        <Button variant="link" className="font-semibold underline p-0">
+                          See All Comments
+                        </Button>
+                      </DialogTrigger>
+                      <DialogContent className="md:max-w-[613px] md:max-h-[902px] h-[90vh] flex flex-col p-0 gap-0 overflow-hidden">
+                        <DialogHeader className="p-6 pb-2 flex-none">
+                          <DialogTitle className="text-xl font-bold">
+                            Comment({dataComments?.length || 0})
+                          </DialogTitle>
+                        </DialogHeader>
 
-                          {!isuser && (
-                            <div className="px-6 py-4 flex flex-col gap-3 border-b bg-white z-10 flex-none">
-                              <LoginBeforeComment pathname={pathname} />
-                            </div>
-                          )}
-
-                          {isuser && (
-                            <div className="px-6 py-4 flex flex-col gap-3 border-b bg-white z-10 flex-none">
-                              <span className="font-semibold text-sm">Give your Comments</span>
-                              <Field data-invalid={!commentValid}>
-                                <Textarea
-                                  value={comment}
-                                  onChange={(e) => handleComment(e.target.value)}
-                                  aria-invalid={!commentValid}
-                                  placeholder="Enter your comment"
-                                  className="h-35 w-full rounded-xl px-4 py-2" />
-                                {!commentValid && (<FieldLabel className="text-xs colorerrormsg" htmlFor="input-invalid">Comment required</FieldLabel>)}
-                              </Field>
-                              <Button
-                                disabled={isPending}
-                                onClick={sendComment}
-                                className="rounded-full w-full max-w-[180px] h-11 self-end"
-                              >
-                                {isPending && <Spinner className="mr-2" />}
-                                Send
-                              </Button>
-                            </div>
-                          )}
-
-
-                          <div
-                            id="commentInDialog"
-                            className="flex-1 overflow-y-auto px-6 no-scrollbar bg-gray-50/30"
-                          >
-                            {dataComments && dataComments.length > 0 && (
-                              dataComments?.map((comment) => (
-                                <div key={comment.id}
-                                  className="grid border-t py-3 text-sm gap-3">
-                                  <div className="flex gap-2 items-center">
-                                    <Image
-                                      src={comment.author.avatarUrl
-                                        ? `${comment.author.avatarUrl}`
-                                        : tmpProfilePicture
-                                      }
-                                      alt={comment.author.name}
-                                      width={40}
-                                      height={40}
-                                      className="rounded-full object-cover w-10 h-10"
-                                    />
-                                    <div className="grid gap-2">
-                                      <b>{comment.author.name}</b>
-                                      <span>{comment.createdAt && (formattedDate(comment.createdAt, 'DD MMMM YYYY'))}</span>
-                                    </div>
-                                  </div>
-                                  <p>{comment.content}</p>
-                                </div>
-                              ))
-                            )}
+                        {!isuser && (
+                          <div className="px-6 py-4 flex flex-col gap-3 border-b bg-white z-10 flex-none">
+                            <LoginBeforeComment pathname={pathname} />
                           </div>
-                        </DialogContent>
-                      </Dialog>
-                    </div>
-                  )}
+                        )}
+
+                        {isuser && (
+                          <div className="mx-6 py-4 flex flex-col gap-3 border-b bg-white z-10 flex-none">
+                            <span className="font-semibold text-sm">Give your Comments</span>
+                            <Field data-invalid={!commentValid}>
+                              <Textarea
+                                value={comment}
+                                onChange={(e) => handleComment(e.target.value)}
+                                aria-invalid={!commentValid}
+                                placeholder="Enter your comment"
+                                className="h-35 w-full rounded-xl px-4 py-2" />
+                              {!commentValid && (<FieldLabel className="text-xs colorerrormsg" htmlFor="input-invalid">Comment required</FieldLabel>)}
+                            </Field>
+                            <Button
+                              disabled={isPending}
+                              onClick={sendComment}
+                              className="rounded-full w-full max-w-[180px] h-11 self-end"
+                            >
+                              {isPending && <Spinner className="mr-2" />}
+                              Send
+                            </Button>
+                          </div>
+                        )}
+
+
+                        <div
+                          id="commentInDialog"
+                          className="flex-1 overflow-y-auto px-6 no-scrollbar bg-gray-50/30"
+                        >
+                          {dataComments && dataComments.length > 0 && (
+                            dataComments?.map((comment) => (
+                              <div key={comment.id}
+                                className="grid border-t py-3 text-sm gap-3">
+                                <div className="flex gap-2 items-center">
+                                  <Image
+                                    src={comment.author.avatarUrl
+                                      ? `${comment.author.avatarUrl}`
+                                      : tmpProfilePicture
+                                    }
+                                    alt={comment.author.name}
+                                    width={40}
+                                    height={40}
+                                    className="rounded-full object-cover w-10 h-10"
+                                  />
+                                  <div className="grid gap-2">
+                                    <b>{comment.author.name}</b>
+                                    <span>{comment.createdAt && (formattedDate(comment.createdAt, 'DD MMMM YYYY'))}</span>
+                                  </div>
+                                </div>
+                                <p>{comment.content}</p>
+                              </div>
+                            ))
+                          )}
+                        </div>
+                      </DialogContent>
+                    </Dialog>
+                  </div>
                 </div>
               </div>
 
